0843d74019f8d8b6e64379d8c34267461cf1eee7
Adam Reed <kusa.xisa@gmail.com>
2017-01-18 16:27:46 -0500

    Added tests for validation functions

---
+++ spec/play_game_of_sticks_spec.rb
@@ -0,0 +1,28 @@
+require 'play_game_of_sticks'
+
+describe '#get_yes_or_no' do
+  context 'When y or n is passed' do
+    it 'Returns the letter entered if it is y or n' do
+      expect(get_yes_or_no('y')).to eq 'y'
+      expect(get_yes_or_no('n')).to eq 'n'
+    end
+  end
+
+  context 'When something else is entered' do
+    it 'Returns an empty string when something else is entered' do
+      expect(get_yes_or_no('s')).to eq ''
+    end
+  end
+end
+
+describe '#play_again?' do
+  context 'Only y or n will be passed due to the above function' do
+    it 'Returns true if y is entered' do
+      expect(play_again?('y')).to be true
+    end
+
+    it 'Returns false when n is entered' do
+      expect(play_again?('n')).to be false
+    end
+  end
+end
